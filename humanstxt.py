5021393f8706e3149e20f56c64795729395bcfec
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-13 17:01:39 -0600

    Added empty API token message

--- humanstxt.py
+++ humanstxt.py
@@ -1,8 +1,14 @@
 import json
 import requests
 
+def authURL(url):
+	if len(auth.strip()) > 0:
+		return url+"?access_token="+auth
+	else:
+		return url
+
 def printUser(url, fil):
-	u = json.loads(requests.get(url).text)
+	u = json.loads(requests.get(authURL(url)).text)
 	fil.write('\n  ')
 	try:
 		if len(u["name"].strip()) == 0:
@@ -27,18 +33,26 @@ def printUser(url, fil):
 		pass
 	fil.write("\n")		
 	print n
+	return True
 
 repo = raw_input("Repository Name (username/repoName): ")
 auth = raw_input("Create a Personal Access Token at https://github.com/settings/tokens/new \nPaste it here: ")
 
-d = json.loads(requests.get("https://api.github.com/repos/"+repo+"/contributors?access_token="+auth).text)
-o = json.loads(requests.get("https://api.github.com/repos/"+repo+"?access_token="+auth).text)
+while len(auth.strip()) == 0:
+	a = raw_input("Are you sure you want to continue without an API token? \nThis will greatly limit the amount of calls able to be made. \n[Y/n]:").lower()
+	if a == "n" or a == "no":
+		auth = raw_input("Paste your Personal Access Token here: ")
+	if a == "y" or a == "yes":
+		break
+
+d = json.loads(requests.get(authURL("https://api.github.com/repos/"+repo+"/contributors")).text)
+o = json.loads(requests.get(authURL("https://api.github.com/repos/"+repo)).text)
 
 with open('humans.txt', 'wb') as f:
 	print "\nOwner of "+repo+":"
 	f.write('/* OWNER */\n')
-	printUser(o["owner"]["url"]+"?access_token="+auth, f)
+	printUser(o["owner"]["url"], f)
 	print "\nContributors to "+repo+" \n(more info will be written to output.txt):"
 	f.write('\n/* CONTRIBUTORS */\n')
 	for i in d:
-		printUser(i["url"]+"?access_token="+auth, f)
+		printUser(i["url"], f):
